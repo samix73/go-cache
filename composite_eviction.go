7cef567316fbca56e1c4e8732d35dabe551a9b02
Saman Yousefisohi <samix.ys@gmail.com>
2026-04-06 22:55:22 +0200

    feat: implement CompositeEvictionStrategy for flexible eviction management

---
+++ composite_eviction.go
@@ -0,0 +1,49 @@
+package cache
+
+var _ EvictionStrategy[string, int] = (*CompositeEvictionStrategy[string, int])(nil)
+
+type CompositeEvictionStrategy[K comparable, V any] struct {
+	strategies []EvictionStrategy[K, V]
+}
+
+func NewCompositeEvictionStrategy[K comparable, V any](strategies []EvictionStrategy[K, V]) *CompositeEvictionStrategy[K, V] {
+	return &CompositeEvictionStrategy[K, V]{
+		strategies: strategies,
+	}
+}
+
+func (c *CompositeEvictionStrategy[K, V]) Evict() (K, bool) {
+	for _, strategy := range c.strategies {
+		key, shouldEvict := strategy.Evict()
+		if shouldEvict {
+			return key, true
+		}
+	}
+
+	var zeroKey K
+	return zeroKey, false
+}
+
+func (c *CompositeEvictionStrategy[K, V]) RecordAccess(key K) {
+	for _, strategy := range c.strategies {
+		strategy.RecordAccess(key)
+	}
+}
+
+func (c *CompositeEvictionStrategy[K, V]) RecordDeletion(key K) {
+	for _, strategy := range c.strategies {
+		strategy.RecordDeletion(key)
+	}
+}
+
+func (c *CompositeEvictionStrategy[K, V]) RecordInsertion(key K) {
+	for _, strategy := range c.strategies {
+		strategy.RecordInsertion(key)
+	}
+}
+
+func (c *CompositeEvictionStrategy[K, V]) Clear() {
+	for _, strategy := range c.strategies {
+		strategy.Clear()
+	}
+}
